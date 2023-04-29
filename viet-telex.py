642fb12d375b43bd6a156bc054e1fd91c211265f
A.J.S. Verbeek <ajsverbeek@gmail.com>
2023-04-29 13:37:48 +0200

    fix: extra instructions

--- viet-telex.py
+++ viet-telex.py
@@ -16,65 +16,63 @@ prompt = PromptTemplate(
     template="""
     The user will send you input and you should return some output.
     
-    Here comes an example of your input: a vietnamese word and its translation:
-    hình dạng	shape
-    phẳng	flat
-    công việc	work
-    kinh nghiệm	experience
-    cột	column
-    nó	it
-    nói	said
-    xe	car
-    giư	hold
-    giai điệu	melody
+    Here comes an example of your input: a vietnamese word and an English word (a Vietnamese word is sometimes 2 words 
+    separated by a space, the English word is always on the right): 
+    
+    | phẳng       | flat       |
+    | công việc   | work       |
+    | kinh nghiệm | experience |
+    | cột         | column     |
+    | nó          | it         |
+    | nói         | said       |
+    | xe          | car        |
+    | giư         | hold       |
+    | giai điệu   | melody     |
     
     The output should be a json object.
     The key should be a Vietnamese word.
     The value is another json object, it contains the following keys: telex, translation, ntd, tm.
-    The telex key should contain a value which is the word that the user should type in order to get the Vietnamese word.
-    The translation key should contain a value which is the English translation.
-    The ntd key should contain an array with the non-tonal diacritics that were used, do not include the tone markings here.
-    The tm key should contain an array with the tone markings that were used, do not include the non-tonal diacritics here.
+    The telex key should contain a value which is the word that the user should type in order to get the Vietnamese word. 
+    E.g. to type phố you should type pho-os: the extra o is for ô and the extra s is for ó, together they combine as ố.
+    The translation key should contain a value which is the English translation:
+    
     {{
       "hình dạng": {{
-        "telex": "hifnh ddajng",
+        "telex": "hinhf ddangj",
         "translation": "shape",
-        "ntd": [ì, ạ],
-        "tm": []
       }},
       "kinh nghiệm": {{
-        "telex": "king nghieejm",
+        "telex": "kinh nghiemej",
         "translation": "experience",
-        "ntd": [ê],
-        "tm": [ẹ]
       }}
     }}
     
-    Here is the table for the non-tonal diacritics:
-    Character 	Keys pressed 	Sample input 	Sample output
-    ă 	aw 	trang-w 	trăng
-    â 	aa 	can-a 	cân
-    đ 	dd 	d-dau-a 	đâu
-    ê 	ee 	d-dem-e 	đêm
-    ô 	oo 	nho-o 	nhô
-    ơ 	ow or [ 	mo-w or m[ 	mơ
-    ư 	uw or w or ] 	tu-w or tw or t] 	tư
+    Here is the table for the non-tonal diacritics (the first row is the header of the table):
     
-    Here is the table for the tone markings:
-    Tone 	Keys added to syllable 	Sample input 	Sample output
-    Ngang (level) 	z or nothing 	ngang 	ngang
-    Huyền (falling) 	f 	huyeenf 	huyền
-    Sắc (rising) 	s 	sawcs 	sắc
-    Hỏi (dipping-rising) 	r 	hoir 	hỏi
-    Ngã (rising glottalized) 	x 	ngax 	ngã
-    Nặng (falling glottalized) 	j 	nawngj 	nặng
+    | Character | Keys pressed | Sample input     | Sample output |
+    |-----------|--------------|------------------|---------------|
+    | ă         | aw           | trang-w          | trăng         |
+    | â         | aa           | can-a            | cân           |
+    | đ         | dd           | d-dau-a          | đâu           |
+    | ê         | ee           | d-dem-e          | đêm           |
+    | ô         | oo           | nho-o            | nhô           |
+    | ơ         | ow or [      | mo-w or m[       | mơ            |
+    | ư         | uw or w or ] | tu-w or tw or t] | tư            |
     
-    For the telex key, add both the non-tonal diacritics and the tone markings.
-    You can append both to the end of the telex word. 
-    E.g. to type phố you should type phoos: the extra o is for ô and the extra s is for ó, together they combine as ố.
+    Here is the table for the tone markings (the first row is the header of the table):
+    
+    | Tone                       | Keys added to syllable | Sample input | Sample output |
+    |----------------------------|------------------------|--------------|---------------|
+    | Ngang (level)              | z or nothing           | ngang        | ngang         |
+    | Huyền (falling)            | f                      | huyeenf      | huyền         |
+    | Sắc (rising)               | s                      | sawcs        | sắc           |
+    | Hỏi (dipping-rising)       | r                      | hoir         | hỏi           |
+    | Ngã (rising glottalized)   | x                      | ngax         | ngã           |
+    | Nặng (falling glottalized) | j                      | nawngj       | nặng          |
+    | ư                          | uw                     | tuw          | tư            |
     
     Here comes the input:
-    {text}
+        {text}
     """,
     input_variables=["text"],
 )
@@ -86,7 +84,9 @@ chat = ChatOpenAI(
 )
 
 chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt])
+final_text = chat_prompt.format_prompt(text=args.text)
 
-print(prompt.template)
+print(final_text.to_string())
+print('Number of tokens used: ', chat.get_num_tokens(final_text.to_string()))
 print(
-    chat(chat_prompt.format_prompt(text=args.text).to_messages()).content)
+    chat(final_text.to_messages()).content)
